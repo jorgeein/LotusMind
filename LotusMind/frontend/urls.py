c8419e22b1fd1dea36c87e8f653ee55de95a16d8
jorgeein <jorgeein.2534@gmail.com>
2023-03-29 22:04:43 -0600

    agregue menu
    Describi y agregue menu en urls, en views y hice el html y el css

--- LotusMind/frontend/urls.py
+++ LotusMind/frontend/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 from . import views
 urlpatterns = [
-    path('miusuario', views.paginausuario)
+    path('miusuario', views.paginausuario),
+    path('menu', views.menu)
 ]
--- LotusMind/frontend/views.py
+++ LotusMind/frontend/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def paginausuario(request):
     return render(request, 'frontend/Perfil_Usuario_Lotusmind.html')
+
+def menu(request):
+    return render(request, 'frontend/menu.html')
